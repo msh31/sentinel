09045cfdd6a92b628495f8ee00b37989b2a73b9d
Marco <180540729+msh31@users.noreply.github.com>
2025-08-26 00:14:12 +0200

    fill sentinel header with includes of other headers

--- include/sentinel/core/sentinel.h
+++ include/sentinel/core/sentinel.h
@@ -2,7 +2,18 @@
 // Created by Marco on 22/08/2025.
 //
 
-#ifndef SENTIINEL_H
-#define SENTIINEL_H
+#ifndef SENTINEL_H
+#define SENTINEL_H
 
-#endif //SENTIINEL_H
+// core functionality
+#include <sentinel/core/logger.h>
+#include <sentinel/common.h>
+#include <sentinel/core/config.h> //not required right now
+
+// frequently used utilities
+#include <sentinel/core/utils/system_helper.h>
+#include <sentinel/core/utils/registry_helper.h>
+
+// TODO: determine popular modules
+
+#endif //SENTINEL_H
